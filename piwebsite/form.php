a7ad51724edaede9a390c1abc17533a4102eab45
Mark Whitehouse <mark.whitehouse@sduhsd.net>
2018-06-04 13:29:56 -0700

    form input

---
+++ piwebsite/form.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+
+  <body>
+
+    <h1>Heading 1</h1>
+
+    Welcome <?php echo $_POST["name"]; ?><br>
+    Your email is <?php echo $_POST["email"]; ?> 
+    
+  </body>
+</html>
