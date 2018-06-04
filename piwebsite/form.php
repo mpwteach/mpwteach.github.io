6f1b6e4f58adfe1cd3b1fe935f38faf63012e42f
Mark Whitehouse <mark.whitehouse@sduhsd.net>
2018-06-04 15:25:06 -0700

    fixed

--- piwebsite/form.php
+++ piwebsite/form.php
@@ -3,10 +3,16 @@
 
   <body>
 
-    <h1>Heading 1</h1>
+    <h1>Arg Test in PHP</h1>
 
-    Welcome <?php echo $_POST["name"]; ?><br>
-    Your email is <?php echo $_POST["email"]; ?> 
+    Arg1: <?php echo $_POST["arg1"]; ?><br>
+    Arg2: <?php echo $_POST["arg2"]; ?>
+
+    <br>
+    
+    <?php
+       system( "/usr/lib/cgi-bin/pi/argtest2 " . $_POST["arg1"] . " " . $_POST["arg2"]); 
+    ?>
     
   </body>
 </html>
