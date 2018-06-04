df873232eaf7609356ecb6c367c3dcf23f21c57c
Mark Whitehouse <mark.whitehouse@sduhsd.net>
2018-06-04 07:37:21 -0700

    fix

--- piwebsite/index.php
+++ piwebsite/index.php
@@ -1,3 +1,15 @@
-<?php
-	phpinfo();
-?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>PHP Example</title>
+  </head>
+
+
+  <body>
+
+    <h1>PHP Example</h1>
+
+    <?php phpinfo(); ?>
+    
+  </body>
+</html>
