288ca429825efb12acc63028f13b82dc6f63e03a
Mark Whitehouse <mark.whitehouse@sduhsd.net>
2018-06-04 22:15:16 -0700

    form input

--- piwebsite/forminput.php
+++ piwebsite/forminput.php
@@ -5,11 +5,13 @@
 
     <h1>Arg Test in PHP</h1>
 
+    <h2>Args entered:</h2>
     Arg1: <?php echo $_POST["arg1"]; ?><br>
     Arg2: <?php echo $_POST["arg2"]; ?>
 
     <br>
-    
+
+    <h2>Calling C program with the args</h2>
     <?php
        system( "/usr/lib/cgi-bin/pi/argtest2 " . $_POST["arg1"] . " " . $_POST["arg2"]); 
     ?>
---
+++ piwebsite/forminput2.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Form Input 2</title>
+  </head>
+
+
+  <body>
+
+    <h1>Form Input 2</h1>
+    <p>Demo of how to take form input and pass it to a C program - all in a single page</p>
+
+    <?php
+       // define variables and set to empty values
+       $arg1 = $arg2 = $result = "";
+
+       if ($_SERVER["REQUEST_METHOD"] == "POST") {
+         $arg1 = test_input($_POST["arg1"]);
+         $arg2 = test_input($_POST["arg2"]);
+         $result = system( "/usr/lib/cgi-bin/pi/argtest2 " . $arg1 . " " . $arg2); 
+       }
+
+       function test_input($data) {
+         $data = trim($data);
+         $data = stripslashes($data);
+         $data = htmlspecialchars($data);
+         return $data;
+       }
+    ?>
+
+    <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+      Arg1: <input type="text" name="arg1"><br>
+      Arg2: <input type="text" name="arg2"><br>
+      <input type="submit">
+    </form>
+
+    <?php
+       echo "<h2>Your Input:</h2>";
+       echo $arg1;
+       echo "<br>";
+       echo $arg2;
+       echo "<br>";
+       
+       echo "<h2>Calling C program</h2>";
+       echo $result;
+       
+     ?>
+    
+  </body>
+</html>
